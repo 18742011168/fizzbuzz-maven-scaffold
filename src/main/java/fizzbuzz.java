cad7b2dcf7ebaa9e0485c62821361ff113e93245
18742011168 <34845501+18742011168@users.noreply.github.com>
2020-01-15 15:01:29 +0800

    Create fizzbuzz.java

---
+++ src/main/java/fizzbuzz.java
@@ -0,0 +1,17 @@
+public class fizzbuzz {
+
+	public String out(int i) {
+		// TODO Auto-generated method stub
+		String result="";
+		
+		if(i%3==0)
+			result+="Fizz";
+		
+		if(i%5==0)
+			result+="Buzz";
+		if(result=="")
+			result+=i;
+		return result;
+	}
+
+}
